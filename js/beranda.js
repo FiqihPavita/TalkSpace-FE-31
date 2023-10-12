4bf0ff6552207637d19d9fe99c515f12145b03b3
FiqihPavitaA <fiqih.pavita@gmail.com>
2023-10-12 15:28:08 +0700

    updated 1

--- js/beranda.js
+++ js/beranda.js
@@ -21,3 +21,55 @@ document.getElementById("testimoniForm").addEventListener("submit", function (ev
       alert("Alamat email tidak terdaftar. Anda tidak dapat mengirimkan testimoni.");
   }
 });
+
+function openPopup() {
+  alert("Terimakasih telah mengisi Testimoni");
+  window.location.href = "summary-testi.html";
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const registrationForm = document.getElementById('testimoni-form');
+
+    registrationForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the form from submitting
+
+        const username = document.getElementById('nama_lengkap').value;
+        const email = document.getElementById('email').value;
+        const paketKonseling = document.getElementById('layanan').value;
+        const review = document.getElementById('review')
+
+        try {
+            fetch('https://talkspace-be-production.up.railway.app/api/testimoni/add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    nama_lengkap: username,
+                    email: email,
+                    layanan: paketKonseling,
+                    review: review 
+                })
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Permintaan gagal dengan status: ' + response.status);
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Handle the response from the backend (e.g., display a success message)
+                alert(data.message);
+            })
+            .catch(error => {
+                // Handle errors, such as network errors or server errors
+                console.error('Fetch error:', error);
+                alert('Terjadi kesalahan. Silakan coba lagi nanti.');
+            });
+        } catch (error) {
+            console.error('Fetch error:', error);
+            alert('Terjadi kesalahan. Silakan coba lagi nanti.');
+        }
+    });
+});
---
+++ js/summary-testi.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const okeButton = document.getElementById("okeButton");
+    okeButton.addEventListener("click", function () {
+      window.location.href = "index.html";
+    });
+  });
+  
